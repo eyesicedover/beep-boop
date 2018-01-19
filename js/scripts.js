a87771ff5d59944b131dd4283a7452d3bc3d1cb0
Stephanie Faber <eyesicedover@gmail.com>
2018-01-19 11:04:35 -0800

    fix to scripts.js to reflect beep and boop specs on range based on user input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,45 @@
-function isZero(num) {
-  if (num.includes("0") === true) {
-    return "Beep!";
-  } else {
-    return "Danger Will Robinson!";
-  }
-}
+// function checker(num) {
+//   for (h = 0; h < num.length; h++) {
+//     if (num[h] === 1) {
+//       return "Boop!";
+//     } else if (num[h] === 0) {
+//       return "Beep!";
+//     } else {
+//       return h;
+//     }
+//   }
+// }
+
+// function mapping(x) {
+//   numArray.map(function(x))
+// }
 
 $(document).ready(function() {
   $("#numberForm").submit(function(event) {
     event.preventDefault();
     var number = $("input#userNumber").val();
-    var result = "";
+    var numInt = parseInt(number);
+    var numArray = [];
+    var result = [];
+
+    for (i = 0; i <=numInt; i++) {
+      var numString = i.toString();
+      numArray.push(numString);
+    }
+    debugger;
+
+    for (h = 0; h < numArray.length; h++) {
+      var stringHolder = numArray[h];
+      if (stringHolder.includes("1") === true) {
+        result[h] = "Boop!";
+      } else if (stringHolder.includes("0") === true) {
+        result[h] = "Beep!";
+      } else {
+        result[h] = h;
+      }
+    }
 
 
-    result = isZero(number);
     $("#output").text(result);
 
   });
